c128f59e7e1645d26ac5206ada67d59eb901ae1c
Daniel Luetgehetmann <luetgehetmann@gmail.com>
2024-01-19 09:13:04 -0800

    Update

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Package sets
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
 
     # Environment/system management
     darwin.url = "github:lnl7/nix-darwin/master";
