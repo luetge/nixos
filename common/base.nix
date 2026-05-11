29d6a2683128b0a10c19df58cafa074c86190985
Daniel Luetgehetmann <luetgehetmann@gmail.com>
2026-05-11 11:36:43 +0200

    Builder

--- common/base.nix
+++ common/base.nix
@@ -30,24 +30,24 @@
       };
     };
 
-    # linux-builder = {
-    #   enable = true;
-    #   ephemeral = true;
-    #   maxJobs = 4;
-    #   config = {
-    #     virtualisation = {
-    #       darwin-builder = {
-    #         diskSize = 40 * 1024;
-    #         memorySize = 8 * 1024;
-    #       };
-    #       cores = 6;
-    #     };
-    #   };
-    #   systems = [
-    #     "x86_64-linux"
-    #     "aarch64-linux"
-    #   ];
-    # };
+    linux-builder = {
+      enable = true;
+      ephemeral = true;
+      maxJobs = 4;
+      config = {
+        virtualisation = {
+          darwin-builder = {
+            diskSize = 40 * 1024;
+            memorySize = 8 * 1024;
+          };
+          cores = 6;
+        };
+      };
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+    };
 
     nixPath = [ "nixpkgs=${nixpkgs}" ];
 
